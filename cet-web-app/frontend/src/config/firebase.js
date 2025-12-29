18a95c715e713a6e0956496e71b8d2d1abdbc0f7
supriyamulik <supriyamulik99@gmail.com>
2025-12-29 11:26:03 +0530

    Fix_Firebase

--- cet-web-app/frontend/src/config/firebase.js
+++ cet-web-app/frontend/src/config/firebase.js
@@ -1,15 +1,17 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyDNYHDJUJ9Agvp7iXnsoLfu2u0gmyrxNro",
-    authDomain: "cet-insights.firebaseapp.com",
-    projectId: "cet-insights",
-    storageBucket: "cet-insights.firebasestorage.app",
-    messagingSenderId: "1034272895377",
-    appId: "1:1034272895377:web:030782a92850ae70fb6a30",
-    measurementId: "G-32NESGF1XT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
